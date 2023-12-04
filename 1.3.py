27e29d8a9b4f047ad85c080260d3950dd1a6be00
LiamUpstoneSmith <liamjake2002@hotmail.co.uk>
2023-12-04 13:34:02 +0000

    04/12/2023 set B Implemented

--- 1.3.py
+++ 1.3.py
@@ -12,7 +12,7 @@ PWD5  --  1268 seconds  --  21.14 mins  --  74fcb3939fb7460f1e6a00a32ca45510ebff
 
 LONGER  --  75734 seconds  --  1262 mins --  21.03 hours  --  88765f800a890eef93767f976eca17f91cbf85ea
 
-4 --  6.51  --  1b6453892473a467d07372d45eb05abc2031647a
+4 --  6.51 seconds --  1b6453892473a467d07372d45eb05abc2031647a
 
 BETTER  --  70725 seconds  --  1178.76 mins  --  19.65 hours  --  3bb9cdac38c5c43cd6cbe4752c40041d58894545
 
@@ -24,12 +24,9 @@ import itertools
 import hashlib as hash
 import time
 
-# Starts timer
-start_time = time.time()
-
 
 # SHA1 hashes given input
-def crypto_hash(thing):
+def sha1_hash(thing):
     hasher = hash.sha1(thing.encode())
     c = hasher.hexdigest()
     return c
@@ -41,13 +38,16 @@ hashed_password = input("\nEnter hashed password:\n")
 
 def set_a(check):
 
+    # Starts timer
+    start_time = time.time()
+
     num_letters = 0
 
     # Check if password is 1 character long
     for combination in itertools.product('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', repeat=1):
         c = ''.join(combination)
         print(c)
-        c = crypto_hash(c)
+        c = sha1_hash(c)
         if c == check:
             answer = ''.join(combination)
             print("-------------------------------\n" + answer + "\n-------------------------------")
@@ -55,8 +55,8 @@ def set_a(check):
             end_time = time.time()
             break
         else:
-            for i in range(1000):  # Hashes combination to 1000th level
-                c = crypto_hash(c)
+            for i in range(1000):  # Hashes combination up to 1000th level
+                c = sha1_hash(c)
                 if c == check:
                     answer = ''.join(combination)
                     print("-------------------------------\n", answer, "Hashed in :", i,
@@ -72,7 +72,7 @@ def set_a(check):
         for combination in itertools.product('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', repeat=2):
             c = ''.join(combination)
             print(c)
-            c = crypto_hash(c)
+            c = sha1_hash(c)
             if c == check:
                 answer = ''.join(combination)
                 print("-------------------------------\n" + answer + "\n-------------------------------")
@@ -80,8 +80,8 @@ def set_a(check):
                 end_time = time.time()
                 break
             else:
-                for i in range(1000):  # Hashes combination to 1000th level
-                    c = crypto_hash(c)
+                for i in range(1000):  # Hashes combination up to 1000th level
+                    c = sha1_hash(c)
                     if c == check:
                         answer = ''.join(combination)
                         print("-------------------------------\n", answer, "Hashed in :", i,
@@ -97,7 +97,7 @@ def set_a(check):
         for combination in itertools.product('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', repeat=3):
             c = ''.join(combination)
             print(c)
-            c = crypto_hash(c)
+            c = sha1_hash(c)
             if c == check:
                 answer = ''.join(combination)
                 print("-------------------------------\n" + answer + "\n-------------------------------")
@@ -105,8 +105,8 @@ def set_a(check):
                 end_time = time.time()
                 break
             else:
-                for i in range(1000):  # Hashes combination to 1000th level
-                    c = crypto_hash(c)
+                for i in range(1000):  # Hashes combination up to 1000th level
+                    c = sha1_hash(c)
                     if c == check:
                         answer = ''.join(combination)
                         print("-------------------------------\n", answer, "Hashed in :", i,
@@ -122,7 +122,7 @@ def set_a(check):
         for combination in itertools.product('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', repeat=4):
             c = ''.join(combination)
             print(c)
-            c = crypto_hash(c)
+            c = sha1_hash(c)
             if c == check:
                 answer = ''.join(combination)
                 print("-------------------------------\n" + answer + "\n-------------------------------")
@@ -130,8 +130,8 @@ def set_a(check):
                 end_time = time.time()
                 break
             else:
-                for i in range(1000):  # Hashes combination to 1000th level
-                    c = crypto_hash(c)
+                for i in range(1000):  # Hashes combination up to 1000th level
+                    c = sha1_hash(c)
                     if c == check:
                         answer = ''.join(combination)
                         print("-------------------------------\n", answer, "Hashed in :", i,
@@ -147,7 +147,7 @@ def set_a(check):
         for combination in itertools.product('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', repeat=5):
             c = ''.join(combination)
             print(c)
-            c = crypto_hash(c)
+            c = sha1_hash(c)
             if c == check:
                 answer = ''.join(combination)
                 print("-------------------------------\n" + answer + "\n-------------------------------")
@@ -155,8 +155,8 @@ def set_a(check):
                 end_time = time.time()
                 break
             else:
-                for i in range(1000):  # Hashes combination to 1000th level
-                    c = crypto_hash(c)
+                for i in range(1000):  # Hashes combination up to 1000th level
+                    c = sha1_hash(c)
                     if c == check:
                         answer = ''.join(combination)
                         print("-------------------------------\n", answer, "Hashed in :", i,
@@ -172,7 +172,7 @@ def set_a(check):
         for combination in itertools.product('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', repeat=6):
             c = ''.join(combination)
             print(c)
-            c = crypto_hash(c)
+            c = sha1_hash(c)
             if c == check:
                 answer = ''.join(combination)
                 print("-------------------------------\n" + answer + "\n-------------------------------")
@@ -180,8 +180,8 @@ def set_a(check):
                 end_time = time.time()
                 break
             else:
-                for i in range(1000):  # Hashes combination to 1000th level
-                    c = crypto_hash(c)
+                for i in range(1000):  # Hashes combination up to 1000th level
+                    c = sha1_hash(c)
                     if c == check:
                         answer = ''.join(combination)
                         print("-------------------------------\n", answer, "Hashed in :", i,
@@ -191,6 +191,61 @@ def set_a(check):
                         break
                     else:
                         num_letters = 0
+
+    # Calculates and prints time taken for algorithm to complete
+    overall_secs = end_time - start_time
+    overall_mins = overall_secs / 60
+    overall_secs = round(overall_secs, 2)
+    overall_mins = round(overall_mins, 2)
+    print("\n", overall_secs, " Seconds to complete")
+    print("\n", overall_mins, " mins to complete")
+
+
+# Checks if code is valid isbn number
+def valid_isbn(check):
+
+    # Split the input into an array (list) of digits
+    d = [int(digit) for digit in check]
+
+    # Checks it is a valid code
+    if len(d) < 5 or len(d) > 6:
+        print("Enter valid 6 digit code.")
+        valid_isbn(check)
+
+    # Checks if valid ISBN num
+    else:
+        if (1 * d[0] + 2 * d[1] + 3 * d[2] + 4 * d[3] + 5 * d[4] + 6 * d[5]) % 7 == 0:
+            return True
+        else:
+            return False
+
+
+def set_b(check):
+
+    # Starts timer
+    start_time = time.time()
+
+    # Checks every combination of length 6
+    for combination in itertools.product('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', repeat=6):
+        c = ''.join(combination)
+        print(c)
+
+        # checks if combination is valid ISBN-6 number
+        if valid_isbn(c):
+            c = sha1_hash(c)
+            if c == check:
+                answer = ''.join(combination)
+                print("-------------------------------\n" + answer + "\n-------------------------------")
+                end_time = time.time()
+                break
+            else:
+                for i in range(1000):  # Hashes combination up to 1000th level
+                    c = sha1_hash(c)
+                    if c == check:
+                        answer = ''.join(combination)
+                        print("-------------------------------\n", answer, "Hashed in :", i,
+                              "\n-------------------------------")
+                        end_time = time.time()
                         break
 
     # Calculates and prints time taken for algorithm to complete
@@ -202,4 +257,4 @@ def set_a(check):
     print("\n", overall_mins, " mins to complete")
 
 
-set_a(hashed_password)
+set_b(hashed_password)
